c7213d066836738fcd9985e243eaae496e46039a
RavulaThrinath <thrinathravual@gmail.com>
2022-05-11 10:49:54 +0530

    responsive code

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var typed = new Typed("#typed", {
   strings: ["Follow us on"],
   typeSpeed: 80,
-  startDelay: 3800,
+  startDelay: 4300,
   loop: false,
 });
+
+const myTimeout = setTimeout(() => {
+  UIkit.notification({
+    message: " <span uk-icon='icon: warning'></span> We are under maintaince",
+    status: "warning",
+    pos: 'top-center',
+    timeout: 2000,
+  });
+}, 7000);
